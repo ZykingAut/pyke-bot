f090423bf34bc2ef53bfbea558fe7638a719c1d4
ZykingAut <sponring.maximilian@student.htlwrn.ac.at>
2020-06-19 10:18:52 +0200

    updated help

--- commands/help.js
+++ commands/help.js
@@ -11,8 +11,8 @@ module.exports = {
         const { commands } = msg.client;
 
         if (!args.length) {
-            data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => command.name).join(', '));
+            data.push('**Here\'s a list of all my commands:**');
+            data.push(commands.map(command => command.name).join(', \n'));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
             return msg.author.send(data, {split: true})
