b14a83ddd14d95493b87b173326b56d5b9dbefe9
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-02 21:21:24 +0200

    edited love.js

--- commands/love.js
+++ commands/love.js
@@ -2,11 +2,11 @@ module.exports = {
     name: 'love',
     description: 'Find out the love between two people!',
     execute(msg) {
-        if (msg.mentions.size !== 2) {
+        if (msg.mentions.users.size !== 2) {
             return msg.channel.send('You need to tag two people to calculate their love!');
         } else {
             const love = Math.floor(Math.random() * 101);
-            return msg.channel.send(`The love between ${msg.mentions[0].nickname} and ${msg.mentions[1].nickname} is ${love}%`);
+            return msg.channel.send(`The love between ${msg.mentions.users[0].nickname} and ${msg.mentions.users[1].nickname} is ${love}%`);
         }
     }
 };
