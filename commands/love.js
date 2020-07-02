bbd12cde18fc62e358831ad12c44490e28a1dc98
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-02 21:08:44 +0200

    edited love.js

--- commands/love.js
+++ commands/love.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'love',
     description: 'Find out the love between two people!',
     execute(msg) {
-        if (msg.mentions.length !== 2) {
+        if (msg.mentions.size !== 2) {
             return msg.channel.send('You need to tag two people to calculate their love!');
         } else {
             const love = Math.floor(Math.random() * 101);
