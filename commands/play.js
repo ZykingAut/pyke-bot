a43a22b861e830b612c577a7162dd37735698441
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-03 03:58:17 +0200

    edited play.js

--- commands/play.js
+++ commands/play.js
@@ -1,9 +1,10 @@
-
+const ytdl = require('ytdl-core');
 
 module.exports = {
     name: 'play',
     description: 'Command to let the bot join a voice channel.',
-    async execute(msg) {
+    async execute(msg, args) {
         const connection = msg.member.voice.channel.join();
+        connection.play(ytdl(args, { filter: "audioonly"} ));
     }
 }
