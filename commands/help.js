74ff35c2b2b995bf9c1d2f99782634db2aeb586e
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-07 21:28:01 +0200

    edited help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
         if (!args.length) {
             commandNames.push(commands.map(command => command.name).join('\n'));
             descriptions.push(commands.map(command => command.description).join('\n'));
-            cooldowns.push(commands.forEach(command => command.cooldown + 's'));
+            cooldowns.push(commands.map(command => command.cooldown + 's').join('\n'));
             embed.setTitle('Help');
             embed.setColor('YELLOW');
             embed.addField('Commands', commandNames, true);
