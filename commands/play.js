5844bc25182fe8f165e0f45d785bfb882248cc29
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-08 03:41:40 +0200

    edited play.js

--- commands/play.js
+++ commands/play.js
@@ -3,8 +3,17 @@ const ytdl = require('ytdl-core');
 module.exports = {
     name: 'play',
     description: 'Command to let the bot join a voice channel.',
+    args: true,
     async execute(msg, args) {
+        const playlist = [];
+        playlist.push(args[0]);
         const connection = await msg.member.voice.channel.join();
-        connection.play(ytdl(args[0], { filter: "audioonly"} ));
+        while (playlist.length !== 0) {
+            const dispatcher = connection.play(ytdl(playlist[0]), {filter: 'audioonly'});
+            dispatcher.on('finish', () => {
+                msg.channel.send('Song finished!');
+                playlist.shift();
+            });
+        }
     }
 }
