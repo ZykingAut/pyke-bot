32cf10188d6eacded6e8c04a6df945d1a1caf442
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-07-08 03:03:00 +0200

    overall changes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ client.on('message', msg => {
 
     if (!msg.content.startsWith(prefix)) return;
 
-    const args = msg.content.slice(prefix.length).split(/ -/);
+    const args = msg.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
