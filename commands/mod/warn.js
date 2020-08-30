50372d307ec21f3d12f69d246bf9db33618c5533
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-08-30 19:20:04 +0200

    changes

---
+++ commands/mod/warn.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'warn',
+    description: 'Command to warn a user from a guild',
+    usage: '<@user> <reason>',
+    guildOnly: true,
+    args: true,
+    cooldown: 5,
+    execute() {
+
+    }
+}
