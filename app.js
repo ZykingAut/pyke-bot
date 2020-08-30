4eb3c8f09f398217db52e8878f234232f53e8544
Sponring Maximilian <sponring.maximilian@student.htlwrn.ac.at>
2020-08-30 19:26:27 +0200

    trying to fix spotify error

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ async function connect(client) {
             } else if (!msg.content.startsWith(prefix)) return;
         } else if (!msg.content.startsWith(prefix)) return;
 
-        args = msg.content.slice(prefix.length).split(/ +/);
+        args = msg.content.slice(prefix.length).split(' ');
         const commandName = args.shift().toLowerCase();
 
         const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
