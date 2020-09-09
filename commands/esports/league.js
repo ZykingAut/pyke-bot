140ad3a7b43078d4e57c725aec851c762adb8eef
Maxi Sponring <zyking@Maxis-MacBook-Pro.local>
2020-09-09 14:09:22 +0200

    tried out panda api

--- commands/esports/league.js
+++ commands/esports/league.js
@@ -1,10 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
 module.exports = {
     name: 'league',
     description: 'Command to get upcoming Esports Games around League of Legends',
     usage: '',
     args: false,
     cooldown: 5,
-    execute() {
+    async execute(msg, args) {
+        const { list } = await fetch(`https://api.pandascore.co/lol/champions?token=${process.env.PANDA}`).then(response => response.json());
+        if (!list.length) {
+            return msg.channel.send('Didnt work :c');
+        }
+        const [answer] = list;
+        return console.log(answer);
 
     }
 }
